59a54afcc71e18eac86a66569d664469914aff80
kunal-kasture <kunal699704@gmail.com>
2026-09-26 04:34:40 +0530

    feat: implemented sqaure & circle component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Sqaure & Circle</h1>
+
+      <div className="square">
+        <div className="circle"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
